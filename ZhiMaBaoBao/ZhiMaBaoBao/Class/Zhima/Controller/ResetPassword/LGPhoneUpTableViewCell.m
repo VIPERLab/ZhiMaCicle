592bc0db147dadfc40eb91be08780f16a5ebd3f8
mac <mac@macdeiMac-2.local>
2016-09-20 10:23:52 +0800

    修改了充值密码

--- ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/ResetPassword/LGPhoneUpTableViewCell.m
+++ ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/ResetPassword/LGPhoneUpTableViewCell.m
@@ -20,6 +20,7 @@
 }
 
 - (void)setupView {
+    self.backgroundColor = [UIColor whiteColor];
     self.titleLabel = [UILabel new];
     self.titleLabel.textColor = [UIColor blackColor];
     self.titleLabel.font = [UIFont systemFontOfSize:15];
@@ -31,7 +32,7 @@
     [self addSubview:_Texf];
     
     _bottomLineView = [UIView new];
-    _bottomLineView.backgroundColor = [UIColor colorFormHexRGB:@"e1e1e1"];
+    _bottomLineView.backgroundColor = [UIColor colorFormHexRGB:@"d9d9d9"];
     [self addSubview:_bottomLineView];
     
 }
@@ -43,7 +44,7 @@
     CGFloat titleWidth = [self.titleLabel.text sizeWithFont:[UIFont systemFontOfSize:15] maxSize:CGSizeMake(100, 15)].width;
     self.titleLabel.frame = CGRectMake(20, 0, titleWidth, CGRectGetHeight(self.frame));
     
-    _Texf.frame = CGRectMake(CGRectGetMaxX(self.titleLabel.frame) + 10, 0, CGRectGetWidth(self.frame) - CGRectGetMaxX(self.titleLabel.frame) - 10, CGRectGetHeight(self.frame));
+    _Texf.frame = CGRectMake(100, 0, CGRectGetWidth(self.frame) - CGRectGetMaxX(self.titleLabel.frame) - 10, CGRectGetHeight(self.frame));
     
     _bottomLineView.frame = CGRectMake(20, CGRectGetHeight(self.frame) - 0.5, ScreenWidth - 20, 0.5);
 }
