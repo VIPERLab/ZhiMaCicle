32c82de0500480d5b23d21d1932d9549cf5a150e
mac <mac@macdeiMac-2.local>
2016-09-30 09:06:37 +0800

    新增FMDBShareManager接口

--- ZhiMaBaoBao/ZhiMaBaoBao/Class/ZhimaChat/Controller/ConversationController.m
+++ ZhiMaBaoBao/ZhiMaBaoBao/Class/ZhimaChat/Controller/ConversationController.m
@@ -97,6 +97,8 @@
     return 0.5;
 }
 
+
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     ConverseModel *model = self.dataArray[indexPath.row];
@@ -106,6 +108,23 @@
     [self.navigationController pushViewController:vc animated:YES];
 }
 
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source.
+        ConverseModel *model = self.dataArray[indexPath.row];
+        //数据库删除该条会话
+        [FMDBShareManager deleteConverseWithConverseId:model.converseId];
+        
+        [self getDataFormSqlist];
+        
+    }
+}
 
 #pragma mark - lazyLoad
 
