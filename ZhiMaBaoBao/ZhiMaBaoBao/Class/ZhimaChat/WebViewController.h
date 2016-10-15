0828c215bc0da8d7f3685b012d455d6383c346f1
liugang <gang@liugangdeiMac.local>
2016-10-15 16:28:42 +0800

    新增网页控制器

---
+++ ZhiMaBaoBao/ZhiMaBaoBao/Class/ZhimaChat/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  ZhiMaBaoBao
+//
+//  Created by liugang on 16/10/15.
+//  Copyright © 2016年 liugang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface WebViewController : BaseViewController
+
+@end
