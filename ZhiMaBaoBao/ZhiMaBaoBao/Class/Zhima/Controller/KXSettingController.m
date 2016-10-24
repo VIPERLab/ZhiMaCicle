50413957128403c0153eb19b382cc2213a58c167
mac <mac@macdeiMac-2.lan>
2016-10-24 17:39:25 +0800

    新增关闭推送

--- ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/KXSettingController.m
+++ ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/KXSettingController.m
@@ -14,6 +14,7 @@
 #import "LGBlackListViewController.h" //黑名单
 #import "NewMessageSettingController.h" //新消息提醒
 #import "PersonalSettingController.h" //隐私设置
+#import "JPUSHService.h"
 
 #import "SocketManager.h"
 
@@ -161,7 +162,12 @@
                 info.hasLogin = NO;
                 [info save];
                 
-                [[SocketManager shareInstance] disconnect];
+                //关闭sorket
+                [[SocketManager shareInstance] disconnect];                
+                
+                //关闭推送
+                [JPUSHService removeNotification:nil];
+                
                 //成功退出
                 [[NSNotificationCenter defaultCenter] postNotificationName:LOGOUT_SUCCESS object:nil];
                 
