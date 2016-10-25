12dfa2e73043d80a0eba637b70055285ecfae661
mac <mac@macdeiMac-2.lan>
2016-10-25 19:06:44 +0800

    修改数据库多线程并发

--- ZhiMaBaoBao/ZhiMaBaoBao/Main/Tools/FMDBManager/FMDBManager.m
+++ ZhiMaBaoBao/ZhiMaBaoBao/Main/Tools/FMDBManager/FMDBManager.m
@@ -1919,31 +1919,35 @@
  *  @param converseID 会话id
  */
 - (void)deleteMessageFormMessageTableByConverseID:(NSString *)converseID {
-    FMDatabaseQueue *messageQueue = [FMDBShareManager getQueueWithType:ZhiMa_Chat_Message_Table];
-    NSString *option = [NSString stringWithFormat:@"converseId = %@",converseID];
-    NSString *detOpeartionStr = [FMDBShareManager deletedTableData:ZhiMa_Chat_Message_Table withOption:option];
-    [messageQueue inDatabase:^(FMDatabase *db) {
-        BOOL success = [db executeUpdate:detOpeartionStr];
-        if (success) {
-            NSLog(@"删除消息成功");
-        } else {
-            NSLog(@"删除消息失败");
-        }
-    }];
+    //异步删除
+    dispatch_sync(dispatch_get_global_queue(0, 0), ^{
+        FMDatabaseQueue *messageQueue = [FMDBShareManager getQueueWithType:ZhiMa_Chat_Message_Table];
+        NSString *option = [NSString stringWithFormat:@"converseId = %@",converseID];
+        NSString *detOpeartionStr = [FMDBShareManager deletedTableData:ZhiMa_Chat_Message_Table withOption:option];
+        [messageQueue inDatabase:^(FMDatabase *db) {
+            BOOL success = [db executeUpdate:detOpeartionStr];
+            if (success) {
+                NSLog(@"删除消息成功");
+            } else {
+                NSLog(@"删除消息失败");
+            }
+        }];
+        
+        //更新会话列表
+        FMDatabaseQueue *converseQueue = [FMDBShareManager getQueueWithType:ZhiMa_Chat_Converse_Table];
+        NSString *optionStr1 = [NSString stringWithFormat:@"converseContent = ' ', unReadCount = '0'"];
+        NSString *optionStr2 = [NSString stringWithFormat:@"converseId = '%@'",converseID];
+        NSString *converseOption = [FMDBShareManager alterTable:ZhiMa_Chat_Converse_Table withOpton1:optionStr1 andOption2:optionStr2];
+        [converseQueue inDatabase:^(FMDatabase *db) {
+            BOOL success = [db executeUpdate:converseOption];
+            if (success) {
+                NSLog(@"更新会话成功");
+            } else {
+                NSLog(@"更新会话失败");
+            }
+        }];
+    });
     
-    //更新会话列表
-    FMDatabaseQueue *converseQueue = [FMDBShareManager getQueueWithType:ZhiMa_Chat_Converse_Table];
-    NSString *optionStr1 = [NSString stringWithFormat:@"converseContent = ' ', unReadCount = '0'"];
-    NSString *optionStr2 = [NSString stringWithFormat:@"converseId = '%@'",converseID];
-    NSString *converseOption = [FMDBShareManager alterTable:ZhiMa_Chat_Converse_Table withOpton1:optionStr1 andOption2:optionStr2];
-    [converseQueue inDatabase:^(FMDatabase *db) {
-        BOOL success = [db executeUpdate:converseOption];
-        if (success) {
-            NSLog(@"更新会话成功");
-        } else {
-            NSLog(@"更新会话失败");
-        }
-    }];
 }
 
 /**
