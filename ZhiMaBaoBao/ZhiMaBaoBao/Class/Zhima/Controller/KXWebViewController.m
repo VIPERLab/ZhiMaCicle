5741129331f5d928144f14b19fc034b025fc4a3d
mac <mac@macdeiMac-2.lan>
2016-10-29 09:46:26 +0800

    修改了网页返回的bug

--- ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/KXWebViewController.m
+++ ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/KXWebViewController.m
@@ -8,20 +8,25 @@
 
 #import "KXWebViewController.h"
 
-@interface KXWebViewController ()
+@interface KXWebViewController () <UIWebViewDelegate>
 
 @end
 
-@implementation KXWebViewController
+@implementation KXWebViewController {
+    UIWebView *_webView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self setCustomTitle:_navTitleName];
+//    [self setCustomTitle:_navTitleName];
+    [self setupNav];
     self.view.backgroundColor = BGCOLOR;
     
     UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    _webView = webView;
+    webView.delegate = self;
     [self.view addSubview:webView];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:_htmlURL]];
@@ -29,19 +34,33 @@
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupNav {
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 120, 40)];
+    titleLabel.text = self.navTitleName;
+    titleLabel.textColor = THEMECOLOR;
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont systemFontOfSize:17];
+    self.navigationItem.titleView = titleLabel;
+    
+    //自定义返回按钮
+    UIButton *backBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    backBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [backBtn setImage:[UIImage imageNamed:@"nav_back"] forState:UIControlStateNormal];
+    [backBtn addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithCustomView:backBtn];
+    self.navigationItem.leftBarButtonItem = backItem;
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)backAction {
+    if ([_webView canGoBack]) {
+        [_webView goBack];
+    }
+    else
+    {
+        [self.navigationController popViewControllerAnimated:YES];
+        
+    }
 }
-*/
 
 @end
