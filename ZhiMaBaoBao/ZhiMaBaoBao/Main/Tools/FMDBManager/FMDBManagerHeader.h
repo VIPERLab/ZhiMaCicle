0dfad1fbe3269cf30c9e90a43cf550c9a808231d
mac <mac@macdeiMac-2.local>
2016-11-07 09:23:05 +0800

    提交代码

--- ZhiMaBaoBao/ZhiMaBaoBao/Main/Tools/FMDBManager/FMDBManagerHeader.h
+++ ZhiMaBaoBao/ZhiMaBaoBao/Main/Tools/FMDBManager/FMDBManagerHeader.h
@@ -39,14 +39,14 @@
 #define ZhiMaCollection_TableName @"Collection "
 
 //  创建朋友圈table字段
-#define CircleField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, friend_nick TEXT NOT NULL, circle_ID INTEGER, userID TEXT NOT NULL, content TEXT NOT NULL, current_location TEXT NOT NULL, create_time TEXT NOT NULL, head_photo TEXT NOT NULL, content_type INTEGER, article_link TEXT)"
+#define CircleField @"( friend_nick TEXT NOT NULL, circle_ID INTEGER, userID TEXT NOT NULL, content TEXT NOT NULL, current_location TEXT NOT NULL, create_time TEXT NOT NULL, head_photo TEXT NOT NULL, content_type INTEGER, article_link TEXT, PRIMARY KEY ('circle_ID'))"
 // 取朋友圈table的字段名
 #define CircleFiels_Name @"friend_nick, circle_ID, userID, content, current_location, create_time, head_photo, content_type, article_link"
 
 
 
 // 创建 --- 评论字段
-#define Circle_CommentField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, friend_nick TEXT NOT NULL, fcid TEXT NOT NULL, comment TEXT NOT NULL, reply_friend_nick TEXT NOT NULL, reply_id TEXT NOT NULL, head_photo TEXT NOT NULL, create_time TEXT NOT NULL, circle_ID TEXT NOT NULL, userID TEXT NOT NULL)"
+#define Circle_CommentField @"( friend_nick TEXT NOT NULL, fcid TEXT NOT NULL, comment TEXT NOT NULL, reply_friend_nick TEXT NOT NULL, reply_id TEXT NOT NULL, head_photo TEXT NOT NULL, create_time TEXT NOT NULL, circle_ID TEXT NOT NULL, userID TEXT NOT NULL)"
 // 取 ----  评论字段 -- 名
 #define Circle_CommentFields_Name @"friend_nick, fcid, comment, reply_friend_nick, reply_id, head_photo, create_time, circle_ID, userID"
 
@@ -55,17 +55,17 @@
 // 创建朋友圈图片 --- 字段名
 #define CirCle_PicField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, img_url TEXT NOT NULL, bigimg_url TEXT NOT NULL, circle_ID TEXT NOT NULL, weuser_id TEXT NOT NULL)"
 // 取朋友圈图片 -- 字段
-#define CirCle_PicFields_Name @"img_url, bigimg_url, circle_ID,weuser_id"
+#define CirCle_PicFields_Name @"img_url, bigimg_url, circle_ID, weuser_id"
 
 
 // 创建朋友圈点赞 ----- 字段名
-#define CirCle_LikeField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT NOT NULL, userId TEXT NOT NULL, userPhoto TEXT NOT NULL, circle_ID TEXT NOT NULL)"
+#define CirCle_LikeField @"( userName TEXT NOT NULL, userId TEXT NOT NULL, userPhoto TEXT NOT NULL, circle_ID TEXT NOT NULL)"
 // 取 ----  点赞字段 -- 名
 #define CirCle_LikeFields_Name @"userName, userId, userPhoto, circle_ID"
 
 
 // 创建 聊天会话 table 字段
-#define Chat_ConverseField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER, converseType INTEGER, converseId TEXT NOT NULL, unReadCount INTEGER, topChat INTEGER, disturb INTEGER, converseName TEXT NOT NULL, converseHead_photo TEXT NOT NULL, converseContent TEXT NOT NULL,serviceMessageType INTEGER)"
+#define Chat_ConverseField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER, converseType INTEGER, converseId TEXT NOT NULL, unReadCount INTEGER, topChat INTEGER, disturb INTEGER, converseName TEXT NOT NULL, converseHead_photo TEXT NOT NULL, converseContent TEXT NOT NULL,serviceMessageType INTEGER, PRIMARY KEY ('converseId'))"
 #define Chat_ConverseFields_Name @"time,converseType,converseId,unReadCount,topChat,disturb,converseName,converseHead_photo,converseContent,serviceMessageType"
 
 
@@ -88,7 +88,7 @@
 #define GroupChat_MessageFields_name @"groupId, groupName, notice, topChat, disturb, saveToMailList, myGroupName, showMemberName, groupAvtar"
 
 //群成员表
-#define GroupChat_MemberField @"(id INTEGER PRIMARY KEY AUTOINCREMENT, converseId TEXT NOT NULL, memberId TEXT NOT NULL, memberName TEXT NOT NULL, memberHeader_Photo TEXT NOT NULL, memberGroupState INTEGER)"
+#define GroupChat_MemberField @"(converseId TEXT NOT NULL, memberId TEXT NOT NULL, memberName TEXT NOT NULL, memberHeader_Photo TEXT NOT NULL, memberGroupState INTEGER)"
 #define GroupChat_MemberFields_Name @"converseId, memberId, memberName, memberHeader_Photo,memberGroupState"
 
 
