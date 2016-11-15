9ed33d7be257996dd462675d4fb2aafa8b084a37
mac <mac@Js-iPhone.lan>
2016-11-15 11:57:58 +0800

    xxx

--- ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/PersonalCenterController.m
+++ ZhiMaBaoBao/ZhiMaBaoBao/Class/Zhima/Controller/PersonalCenterController.m
@@ -64,9 +64,7 @@
     ViciterLoginView *viciter = [[ViciterLoginView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight)];
     _viciterView = viciter;
     [viciter setLoginBlock:^{
-//        [self loginButtonDidClick:nil];
         [[NSNotificationCenter defaultCenter] postNotificationName:kPressentLoginRegiste object:nil];
-
     }];
     [self.view addSubview:viciter];
     
